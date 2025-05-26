fdec0f1eda272a6c74a267ec7f76e95ed2421b5b
LeHuman <47549331+LeHuman@users.noreply.github.com>
2025-05-26 12:04:33 -0700

    chore: :wrench: Initial commit

---
+++ deploy.ps1
@@ -0,0 +1,14 @@
+# Paths
+$source = "F:\GitHub\dostest\build\DOSTest.exe"
+$destination = "D:\APPS\DOSTest.exe"
+$driveLetter = "D"
+
+Write-Host "Copying $source to $destination..."
+Copy-Item -Path $source -Destination $destination -Force
+
+Start-Sleep -Seconds 0.5
+
+Write-Host "Ejecting drive $driveLetter..."
+
+$driveEject = New-Object -comObject Shell.Application
+$driveEject.Namespace(17).ParseName("${driveLetter}:\").InvokeVerb("Eject")
